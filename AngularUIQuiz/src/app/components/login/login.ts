c64eab9dba45ca84d0a5c904bf137fcf81ed678d
Intuon <oonoon1994@gmail.com>
2025-11-23 02:46:16 +0700

    validateform

--- AngularUIQuiz/src/app/components/login/login.ts
+++ AngularUIQuiz/src/app/components/login/login.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from "@angular/router";
+import ValidateForm from '../../helpers/validateform';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [
+    RouterLink, 
+    ReactiveFormsModule, 
+    CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  loginForm!: FormGroup;
+
+  constructor(private fb:FormBuilder){}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['' , Validators.required]
+    });
+  }
+
+  onSubmit(){
+
+    if(this.loginForm.valid){
+
+    }else{
+      ValidateForm.validateAllFormFileds(this.loginForm);
+    }
+  }
 }
--- AngularUIQuiz/src/app/components/register/register.ts
+++ AngularUIQuiz/src/app/components/register/register.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import ValidateForm from '../../helpers/validateform';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [CommonModule , ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  
+  registerForm!:FormGroup
+  constructor(private fb:FormBuilder){}
 
+  ngOnInit(): void {
+      this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['' , Validators.required],
+      confirmpassword: ['' , Validators.required]
+    });
+  }
+
+  onRegister(){
+
+    if(this.registerForm.valid){
+
+    }else{
+     ValidateForm.validateAllFormFileds(this.registerForm);
+    }
+  }
 }
