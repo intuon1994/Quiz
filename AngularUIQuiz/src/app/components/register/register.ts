62b8815a57b6f2dd977ad38da8bcea930e201af9
Intuon <oonoon1994@gmail.com>
2025-11-23 14:54:26 +0700

    confirmpassword

--- AngularUIQuiz/src/app/components/register/register.ts
+++ AngularUIQuiz/src/app/components/register/register.ts
@@ -5,29 +5,46 @@ import ValidateForm from '../../helpers/validateform';
 
 @Component({
   selector: 'app-register',
-  imports: [CommonModule , ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
 export class RegisterComponent implements OnInit {
-  
-  registerForm!:FormGroup
-  constructor(private fb:FormBuilder){}
 
-  ngOnInit(): void {
-      this.registerForm = this.fb.group({
+  registerForm!: FormGroup;
+
+constructor(private fb: FormBuilder) { }
+
+ngOnInit(): void {
+  this.registerForm = this.fb.group(
+    {
       username: ['', Validators.required],
-      password: ['' , Validators.required],
-      confirmpassword: ['' , Validators.required]
-    });
-  }
+      password: ['', Validators.required],
+      confirmpassword: ['', Validators.required]
+    },
+    { validators: this.passwordMatchValidator }
+  );
+}
 
-  onRegister(){
+passwordMatchValidator(form: FormGroup) {
+  const password = form.get('password')?.value;
+  const confirmPassword = form.get('confirmpassword')?.value;
 
-    if(this.registerForm.valid){
-
-    }else{
-     ValidateForm.validateAllFormFileds(this.registerForm);
+  if (password !== confirmPassword) {
+    form.get('confirmpassword')?.setErrors({ mismatch: true });
+  } else {
+    if (form.get('confirmpassword')?.errors?.['mismatch']) {
+      form.get('confirmpassword')?.setErrors(null);
     }
   }
+}
+
+onRegister() {
+  if (this.registerForm.valid) {
+    console.log("Register data:", this.registerForm.value);
+  } else {
+    ValidateForm.validateAllFormFileds(this.registerForm);
+  }
+}
+
 }
